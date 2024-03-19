52a2cb3611dd27654d816bccd25f76b7ba32f0d9
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-03-19 15:40:32 +0700

    prosedur & fungsi

--- KenapaBandung.cpp
+++ KenapaBandung.cpp
@@ -3,22 +3,22 @@ using namespace std;
 
 
 int main(){
-
     int i;
-    string nama[5]
 
-    for(i = 0; i < 7; i++){
-        cout << i << "i " << "kenapa bandung" << endl;
+    string nama[5];
+
+    for(i = 7; i > 0; i--){
+        cout << i << " i " << "kenapa bandung" << endl;
     }
 
-    for (i=0;i<5; i++){
+    for (i = 0; i < 5; i++){
     cout << "masukan nama =";
     cin >> nama[i];
 
     }
 
     cout << endl;
-    cout << "===========================" endl;
+    cout << "==================" << endl;
     cout << "KUMPULAN NAMA-NAMA" << endl;
 
     for (i=0; i < 5; i++){
---
+++ KenapaBandung4.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#using namespace  std;
+
+string nama;
+int nAdidas, nPuma, nNB, nCompass, nNike;
+int hAdidas = 300, hNB = 250, hPuma = 150, hNIke = 350;
+
+void input(){
+    cout << "Masukan jumlah Adidas";
+    cin >> nAdidas;
+
+    cout << "Masukan jumlah Puma";
+    cin >> nPuma;
+
+    cout << "Masukan jumlah New Balance";
+    cin >> nNB;
+
+    cout << "Masukan jumlah Compass";
+    cin >> nCompass;
+
+    cout << "Masukan jumlah Nike";
+    cin >> nNike;
+}
+
+int totalHarga(){
+    return (nAdidas * hAdidas) + (hNB * nNB) + (hPuma * nPuma) + (hNike * nNike);
+}
+
+void display(){
+    cout << endl;
+    cout << "=======================" << endl;
+    cout << endl;
+    cout << "jumlah Puma = " << nPuma << endl;
+    cout << "jumlah Adidas =" << nAdidas << endl;
+    cout << endl;
+    cout << "Total Harga =  Rp." << totalHarga() << endl;
+    }
+
+int main(){
+    input();
+    display();
+}
