95eeafa5a210091b0711beeb06026311c087383f
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-03-19 15:51:39 +0700

    bikin DO WHILE

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int i;
+
+    string nama[5]
+
+    for(i = 7; i > 0; i--){
+        cout << i << " i " << "kenapa bandung" << endl;
+    }
+
+    for (i = 0; i < 5; i++){
+    cout << "masukan nama =";
+    cin >> nama[i];
+
+    }
+
+    cout << endl;
+    cout << "===========================" endl;
+    cout << "KUMPULAN NAMA-NAMA" << endl;
+
+    for (i=0; i < 5; i++){
+        cout << "namanya adalah" << nama [i] << endl;
+    }
+}
